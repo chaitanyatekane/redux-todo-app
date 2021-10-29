3f270a731945983f2ab4d4a6af9ff24148f11b33
Chaitanya Santosh Tekane <chaitanyatekne5@gmail.com>
2021-10-29 07:51:07 +0530

    created UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello Chaitanya</div>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Todo List App in Redux</h2>
+        <form>
+          <input
+            placeholder="Enter Todo"
+            style={{
+              width: 350,
+              padding: 10,
+              borderRadius: 10,
+              border: "none",
+              fontSize: 20,
+            }}
+          />
+          <button
+            type="submit"
+            style={{
+              padding: 12,
+              borderRadius: 10,
+              fontSize: 15,
+              marginLeft: 20,
+            }}
+          >
+            Go
+          </button>
+        </form>
+        <ul className="allTodos">
+          <li className="singleTodo">
+            <span className="todoText">First Todo</span>
+            <button
+              style={{
+                borderRadius: 15,
+                padding: 10,
+                border: "1px solid white",
+                backgroundColor: "orangered",
+              }}
+            >
+              Delete
+            </button>
+          </li>
+        </ul>
+      </header>
+    </div>
+  );
 }
 
 export default App;
